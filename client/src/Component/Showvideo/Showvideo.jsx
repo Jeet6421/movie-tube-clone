72ce25ae6cf3f6f484ccd1c5bc9182581bd3ab32
Jeet6421 <111728525+Jeet6421@users.noreply.github.com>
2025-06-06 11:55:38 +0530

    this is just commit

--- client/src/Component/Showvideo/Showvideo.jsx
+++ client/src/Component/Showvideo/Showvideo.jsx
@@ -8,7 +8,7 @@ const Showvideo = ({vid}) => {
   return (
     <>
     <Link to={`/videopage/${vid._id}`}>
-        <video src={`http://localhost:5000/${vid.filepath}`} className='video_ShowVideo'/>
+        <video src={`https://movie-tube-clone.onrender.com/${vid.filepath}`} className='video_ShowVideo'/>
     </Link>
     <div className="video_description">
         <div className="Chanel_logo_App">
